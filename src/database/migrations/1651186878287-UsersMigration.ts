62ed2fc3a7fd39476f44e45810a6cc4deba411dd
Matheus <matheus.pgm2000@gmail.com>
2022-04-28 20:43:02 -0300

    create migration users

---
+++ src/database/migrations/1651186878287-UsersMigration.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class UsersMigration1651186878287 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "Nome",
+            type: "varchar",
+          },
+          {
+            name: "data_nascimento",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "senha",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
