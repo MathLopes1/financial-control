ca48ee4810e6b6c0c52250c67017e0ff3d5a4182
Matheus <matheus.pgm2000@gmail.com>
2022-04-29 19:44:20 -0300

    feat: creating interface and spending model

---
+++ src/app/entities/SpendModel.ts
@@ -0,0 +1,45 @@
+import {
+  Entity, Column, PrimaryColumn,
+} from "typeorm";
+import { v4 as uuid } from 'uuid';
+import { ISpend } from "../Interfaces/Spend/ISpend";
+
+    @Entity('spend')
+class SpendModel implements ISpend {
+      constructor(
+        entretenimento: number,
+        alimentacao: number,
+        educacao: number,
+        saude: number,
+        transporte: number,
+      ) {
+        if (!this.id) {
+          this.id = uuid();
+        }
+        this.entretenimento = entretenimento;
+        this.alimentacao = alimentacao;
+        this.educacao = educacao;
+        this.saude = saude;
+        this.transporte = transporte;
+      }
+
+        @PrimaryColumn()
+      public id: string;
+
+        @Column()
+        public entretenimento: number;
+
+        @Column()
+        public alimentacao: number;
+
+        @Column()
+        public educacao: number;
+
+        @Column()
+        public saude: number;
+
+        @Column()
+        public transporte: number;
+    }
+
+export default SpendModel;
